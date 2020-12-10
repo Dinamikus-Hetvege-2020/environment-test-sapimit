652d23a988377d206327f5542452e5b371ddcdf4
sapimit <sapimit@gmail.com>
2020-12-10 18:59:32 +0000

    Problem solved

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
-int main()
-{
-  // TO DO - solve the problem
-  return 0;
+using namespace std;
+
+int main() {
+
+	unsigned int number1, number2;
+	cin >> number1 >> number2;
+	cout << number1 + number2;
+
+	return 0;
 }
